544408bb1699e7d4e160cb295ba6bde6eaac2fef
Vaibhav Kurmi <vaibhavkurmi786@gmail.com>
2025-06-07 15:40:08 +0530

    learned how to use multi line comment

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#this is a single line comment
+# this is a single line comment
+
+"""This is a multi line comment
+ now i can write anything
+in any number of lines
+that i need to because this way i dont have to
+use # again and again to comment
+each and every line in a single go
+
+
+"""
